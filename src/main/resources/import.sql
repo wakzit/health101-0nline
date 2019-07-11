965c4ffb5e92ec7af1f2de0fc647cd748e76747d
wakzit <wakzit@gmail.com>
2019-07-11 14:34:43 +0200

    my first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- You can use this file to load seed data into the database using SQL statements
+insert into MemberHibernate4Demo (id, name, email, phone_number, address) values (0, 'Khadijah McBride', 'khadijah.mcBride@jumpco.io', '0114311666', 'Northcliff JHB') 
+insert into MemberHibernate4Demo (id, name, email, phone_number, address) values (1, 'Thapelo Sithole', 'thapelo.sithole@jumpco.io', '0114311666', 'Sasolburg JHB') 
+insert into MemberHibernate4Demo (id, name, email, phone_number, address) values (2, 'Anele Dlongwane', 'anele.dlongwane@jumpco.io', '0114311666', 'Kenilworth CPT') 
+insert into MemberHibernate4Demo (id, name, email, phone_number, address) values (3, 'Clearance Baloyi', 'clearance.baloyi@jumpco.io', '0114311666', 'Ennerdale JHB') 
